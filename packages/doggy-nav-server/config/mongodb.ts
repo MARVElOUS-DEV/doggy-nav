c3f47fdd369da48e91022082624c5e3aeb662f7f
Fang Kai <kai.fang@thoughtworks.com>
2022-07-10 11:12:45 +0800

    feat: support cloud mongodb

--- packages/doggy-nav-server/config/mongodb.ts
+++ packages/doggy-nav-server/config/mongodb.ts
@@ -1,3 +1,4 @@
+const isCloudUrl =  process.env.MONGO_URL && /^mongodb.*:\/\//i.test(process.env.MONGO_URL)
 export default {
-  mongoUrl: `mongodb://${process.env.MONGO_URL|| "127.0.0.1:27017"}/navigation`,
+  mongoUrl: isCloudUrl? process.env.MONGO_URL : `mongodb://${process.env.MONGO_URL|| "127.0.0.1:27017"}/navigation`,
 };
