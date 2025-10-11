721fc70891437956879e02f4b6bc837fec0ee771
fang.kai <fang.kai@cestc.cn>
2025-10-11 16:14:47 +0800

    feat: enhance Nginx configuration for API and update footer copyright

--- packages/doggy-nav-admin/src/components/Footer/index.tsx
+++ packages/doggy-nav-admin/src/components/Footer/index.tsx
@@ -1,9 +1,8 @@
 import { DefaultFooter } from '@ant-design/pro-layout';
 export default () => {
-  const defaultMessage = '狗狗导航';
   return (
     <DefaultFooter
-      copyright={`2022 ${defaultMessage}`}
+      copyright={process?.env?.COPY_RIGHT_TEXT??`2025 狗狗导航`}
       links={[]}
     />
   );
