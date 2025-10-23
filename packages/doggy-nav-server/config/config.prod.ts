23c162e0f9ffa099765979a11b10d3806e14f9bc
fang.kai <fang.kai@cestc.cn>
2025-10-23 18:47:26 +0800

    optimize: optimize eggjs workers in docker env

--- packages/doggy-nav-server/config/config.prod.ts
+++ packages/doggy-nav-server/config/config.prod.ts
@@ -1,6 +1,13 @@
 import { EggAppConfig, PowerPartial } from 'egg';
-
+import os from 'os';
+// 根据环境变量或默认值设置
 export default () => {
+  const workers = process.env.EGG_WORKERS || Math.min(os.cpus().length, 2); // 最多2个worker
   const config: PowerPartial<EggAppConfig> = {};
+  config.cluster = {
+    listen: {
+      workers: parseInt(workers, 10),
+    },
+  };
   return config;
 };
